16c62b21b502a6c18de9fbb77f11bc1c4c725b69
jboegeholz <boegeholz.joern@gmail.com>
2021-01-19 13:27:25 +0100

    refactor

--- 01_simple_scriptable_widget.js
+++ 01_simple_scriptable_widget.js
@@ -1,14 +1,20 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-purple; icon-glyph: genderless;
+// icon-color: deep-gray; icon-glyph: magic;
 
-let widget = createWidget(followers, avatar, stat);
+let widget = await createWidget();
+
+if (!config.runsInWidget) {
+    await widget.presentSmall();
+}
 
 Script.setWidget(widget);
 Script.complete();
 
-function createWidget(followers, avatar, stat) {
+async function createWidget() {
     let widget = new ListWidget();
     widget.backgroundColor = Color.black();
+    let header = widget.addText("Hello World");
+    header.font = Font.mediumSystemFont(20);
     return widget;
 }
