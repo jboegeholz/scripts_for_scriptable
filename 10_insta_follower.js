3c1fdc207e8ff8876b96549e91f706d9f7a5c742
jboegeholz <boegeholz.joern@gmail.com>
2021-01-19 14:05:04 +0100

    add ig follower

---
+++ 10_insta_follower.js
@@ -0,0 +1,39 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: magic;
+
+
+let url = "https://www.instagram.com/prestissimo_guitar/";
+let req = new Request(url);
+let html = await req.loadString();
+
+let followers = extractFollowers(html);
+
+let widget = await createWidget(followers);
+
+if (!config.runsInWidget) {
+    await widget.presentSmall();
+}
+
+Script.setWidget(widget);
+Script.complete();
+
+async function createWidget(followers) {
+    let widget = new ListWidget();
+    widget.backgroundColor = Color.black();
+    let wFollowers = widget.addText(followers);
+    wFollowers.font = Font.mediumRoundedSystemFont(detailFontSize)
+    wFollowers.textColor = detailColor;
+    wFollowers.centerAlignText();
+    return widget;
+}
+
+function extractFollowers(html) {
+    let followersStart = html.indexOf('"edge_followed_by":{"count":');
+    let followersEnd = html.indexOf('},"fbid"', followersStart + 1);
+    let followers = html.substring(followersStart + 28, followersEnd);
+    console.log("followersStart: " + followersStart)
+    console.log("followersEnd: " + followersEnd)
+    console.log("followers: " + followers)
+    return followers;
+}
